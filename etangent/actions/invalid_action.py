bd6565eb97139aec615f272ce56fc6f79b1b7d13
liukuikun <liukuikun@pjlab.org.cn>
2023-08-20 02:50:34 +0000

    [Feat] support AutoGPT

--- etangent/actions/invalid_action.py
+++ etangent/actions/invalid_action.py
@@ -1,20 +1,40 @@
+from typing import Optional
+
 from etangent.actions.base_action import BaseAction
 from etangent.schema import ActionReturn, ActionStatusCode, ActionValidCode
 
 
 class InvalidAction(BaseAction):
+    """This is a invalid action class, which is used to return error message
+    when the action is invalid.
+
+    Args:
+        err_msg (str): The error message. Defaults to 'The action is invalid,
+            please check the action name'.
+
+    Returns:
+        ActionReturn: The action return.
+    """
 
     def __init__(self,
-                 enable=False,
-                 description='你的返回格式不符合要求，请按照要求的格式回复',
-                 **kwargs):
+                 err_msg:
+                 str = 'The action is invalid, please check the action name.',
+                 **kwargs) -> None:
 
-        super().__init__(enable=enable, description=description, **kwargs)
+        super().__init__(enable=False, **kwargs)
+        self._err_msg = err_msg
 
-    def __call__(self, parameter):
+    def __call__(self, err_msg: Optional[str] = None):
+        """Return the error message.
+
+        Args:
+            err_msg (str, optional): The error message. If err_msg is not None,
+                it will be returned, otherwise the default error message will
+                be returned. Defaults to None.
+        """
         action_return = ActionReturn(
             url=None,
-            args=dict(text=parameter),
+            args=dict(text=err_msg),
             errmsg=self._description,
             type=self.name,
             valid=ActionValidCode.INVALID,
