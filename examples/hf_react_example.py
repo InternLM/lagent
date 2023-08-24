4d3d894226d8dfab19206518eb6f01083acbba10
Wenwei Zhang <40779233+ZwwWayne@users.noreply.github.com>
2023-08-24 11:14:21 +0800

    [Fix]: fix importlib issue and hf internlm (#25)
    fix importlib issue and hf internlm

--- examples/hf_react_example.py
+++ examples/hf_react_example.py
@@ -4,7 +4,7 @@ from lagent.agents.react import ReAct
 from lagent.llms.huggingface import HFTransformer
 
 model = HFTransformer(
-    path='internlm/internlm-chat-7b-v1.1',
+    path='internlm/internlm-chat-7b-v1_1',
     meta_template=[
         dict(role='system', begin='<|System|>:', end='<TOKENS_UNUSED_2>\n'),
         dict(role='user', begin='<|User|>:', end='<eoh>\n'),
--- lagent/utils/package.py
+++ lagent/utils/package.py
@@ -1,8 +1,8 @@
-import importlib
+from importlib.util import find_spec
 
 
 def is_module_exist(module_name):
-    spec = importlib.util.find_spec(module_name)
+    spec = find_spec(module_name)
     if spec is None:
         return False
     else:
