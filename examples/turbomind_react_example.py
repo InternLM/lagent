32905583f73c2ffce38b6a121016cf1db9a62dea
liujiangning <liujiangning@pjlab.org.cn>
2024-01-30 22:30:58 +0800

    update example of turbomind_react

--- examples/turbomind_react_example.py
+++ examples/turbomind_react_example.py
@@ -3,36 +3,37 @@ import argparse
 from lagent.actions.action_executor import ActionExecutor
 from lagent.actions.python_interpreter import PythonInterpreter
 from lagent.agents.react import ReAct
-from lagent.llms.lmdeploy import TurboMind
+from lagent.llms.lmdepoly_wrapper import LMDeployPipeline
+from lagent.llms.meta_template import INTERNLM2_META as META
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str, help="The path to the model")
+    parser.add_argument('--path', type=str, help='The path to the model')
     args = parser.parse_args()
     return args
 
+
 if __name__ == '__main__':
     args = parse_args()
-    model = TurboMind(
+    model = LMDeployPipeline(
         path=args.path,
-        meta_template=[
-            dict(role='system', begin='<|System|>:', end='<TOKENS_UNUSED_2>\n'),
-            dict(role='user', begin='<|User|>:', end='<eoh>\n'),
-            dict(role='assistant', begin='<|Bot|>:', end='<eoa>\n', generate=True)
-        ],
-    )
+        meta_template=META,
+        top_p=0.8,
+        top_k=100,
+        temperature=0,
+        repetition_penalty=1.0,
+        stop_words=['<|im_end|>'])
 
     chatbot = ReAct(
         llm=model,
         action_executor=ActionExecutor(actions=[PythonInterpreter()]),
     )
 
-
     def input_prompt():
         print('\ndouble enter to end input >>> ', end='')
         sentinel = ''  # ends when this string is seen
         return '\n'.join(iter(input, sentinel))
-
 
     while True:
         try:
