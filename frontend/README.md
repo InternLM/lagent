71be68bb3481b5c84bfdf9acbe1671b83760b406
fangfang <fangfang@pjlab.org>
2024-07-26 15:12:37 +0800

    feat: frontend init version

---
+++ frontend/README.md
@@ -0,0 +1,14 @@
+## 前端使用
+### 准备node.js开发环境
+如需安装环境，可参照：https://nodejs.org/zh-cn/download/package-manager
+如有，跳转下一步
+
+### 安装依赖
+npm install
+
+### 启动
+npm start
+
+### 知悉
+- 前端服务基于react开发，如需了解react相关知识，可参考：https://react.dev/
+- 如果需要接入后端服务地址，请至src/config/cgi.ts中修改请求链接
---
+++ frontend/src/locale/en.ts
@@ -0,0 +1,21 @@
+export const EN = {
+  // Global
+
+  login: "Login",
+  register: "Register",
+  loginout: "Login Out",
+  userCenter: "Profile",
+
+  welcome: "Hello",
+
+  // Functional
+
+  "nav.home": "Home",
+  "nav.content": "Content",
+
+  // ErrMsg
+  "err.networkError": "Network Error",
+  "err.timeout": "Timeout",
+  "err.overdue":
+    "The current user identity has expired, please log in again if necessary",
+};
