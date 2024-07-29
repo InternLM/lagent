c9b4d7f343f98b45a484d2db143753c9787e2522
liujiangning <liujiangning@pjlab.org.cn>
2024-07-29 13:47:18 +0800

    refactor: BaseBrowser

--- lagent/actions/duckduckgo_browser.py
+++ lagent/actions/duckduckgo_browser.py
@@ -5,18 +5,16 @@ import time
 import warnings
 from typing import List, Optional, Type
 
-import requests
 from cachetools import TTLCache, cached
+from duckduckgo_search import DDGS
 
 from lagent.actions.base_browser import BaseBrowser
 from lagent.actions.parser import BaseParser, JsonParser
 
 
-class BingSearch:
+class DuckDuckGoSearch:
 
     def __init__(self,
-                 api_key: str,
-                 region: str = 'zh-CN',
                  topk: int = 3,
                  black_list: List[str] = [
                      'enoN',
@@ -24,8 +22,6 @@ class BingSearch:
                      'bilibili.com',
                      'researchgate.net',
                  ]):
-        self.api_key = api_key
-        self.market = region
         self.topk = topk
         self.black_list = black_list
 
@@ -33,7 +29,7 @@ class BingSearch:
     def search(self, query: str, max_retry: int = 3) -> dict:
         for attempt in range(max_retry):
             try:
-                response = self._call_bing_api(query)
+                response = self._call_ddgs(query, timeout=20)
                 return self._parse_response(response)
             except Exception as e:
                 logging.exception(str(e))
@@ -41,36 +37,19 @@ class BingSearch:
                     f'Retry {attempt + 1}/{max_retry} due to error: {e}')
                 time.sleep(random.randint(2, 5))
         raise Exception(
-            'Failed to get search results from Bing Search after retries.')
+            'Failed to get search results from DuckDuckGo after retries.')
 
-    def _call_bing_api(self, query: str) -> dict:
-        endpoint = 'https://api.bing.microsoft.com/v7.0/search'
-        params = {'q': query, 'mkt': self.market, 'count': f'{self.topk * 2}'}
-        headers = {'Ocp-Apim-Subscription-Key': self.api_key}
-        response = requests.get(endpoint, headers=headers, params=params)
-        response.raise_for_status()
-        return response.json()
+    def _call_ddgs(self, query: str, **kwargs) -> dict:
+        ddgs = DDGS(**kwargs)
+        response = ddgs.text(query.strip("'"), max_results=10)
+        return response
 
     def _parse_response(self, response: dict) -> dict:
-        webpages = {
-            w['id']: w
-            for w in response.get('webPages', {}).get('value', [])
-        }
         raw_results = []
-
-        for item in response.get('rankingResponse',
-                                 {}).get('mainline', {}).get('items', []):
-            if item['answerType'] == 'WebPages':
-                webpage = webpages.get(item['value']['id'])
-                if webpage:
-                    raw_results.append(
-                        (webpage['url'], webpage['snippet'], webpage['name']))
-            elif item['answerType'] == 'News' and item['value'][
-                    'id'] == response.get('news', {}).get('id'):
-                for news in response.get('news', {}).get('value', []):
-                    raw_results.append(
-                        (news['url'], news['description'], news['name']))
-
+        for item in response:
+            raw_results.append(
+                (item['href'], item['description']
+                 if 'description' in item else item['body'], item['title']))
         return self._filter_results(raw_results)
 
     def _filter_results(self, results: List[tuple]) -> dict:
@@ -90,12 +69,11 @@ class BingSearch:
         return filtered_results
 
 
-class BingBrowser(BaseBrowser):
+class DuckDuckGoBrowser(BaseBrowser):
     """Wrapper around the Web Browser Tool.
     """
 
     def __init__(self,
-                 api_key: str,
                  timeout: int = 5,
                  black_list: Optional[List[str]] = [
                      'enoN',
@@ -103,11 +81,9 @@ class BingBrowser(BaseBrowser):
                      'bilibili.com',
                      'researchgate.net',
                  ],
-                 region: str = 'zh-CN',
                  topk: int = 20,
                  description: Optional[dict] = None,
                  parser: Type[BaseParser] = JsonParser,
                  enable: bool = True):
-        self.searcher = BingSearch(
-            api_key, black_list=black_list, topk=topk, region=region)
+        self.searcher = DuckDuckGoSearch(black_list=black_list, topk=topk)
         super().__init__(timeout, description, parser, enable)
