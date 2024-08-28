906845f1af47fcb7d81c5c32ec44b0cc22204f8a
WangXinXin <winer632@qq.com>
2024-08-28 15:06:00 +0800

    增加对商汤科技商汤商量系列模型的支持（已经和MindSearch项目一起测试OK）   Add support for SenseTime's SenseNova series LLM  (Has been tested OK with MindSearch project) (#234)
    * add sensenova.py
    
    * fix
    
    * optimized
    
    * 测试OK  并上传到PyPI,对应包名和版本号是xlagent==0.2.1
    
    * Update lagent/version.py
    
    Co-authored-by: liukuikun <24622904+Harold-lkk@users.noreply.github.com>
    
    * Update setup.py
    
    Co-authored-by: liukuikun <24622904+Harold-lkk@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: liukuikun <24622904+Harold-lkk@users.noreply.github.com>

--- lagent/llms/__init__.py
+++ lagent/llms/__init__.py
@@ -4,10 +4,11 @@ from .huggingface import HFTransformer, HFTransformerCasualLM, HFTransformerChat
 from .lmdeploy_wrapper import LMDeployClient, LMDeployPipeline, LMDeployServer
 from .meta_template import INTERNLM2_META
 from .openai import GPTAPI
+from .sensenova import SENSENOVA_API
 from .vllm_wrapper import VllmModel
 
 __all__ = [
-    'BaseModel', 'BaseAPIModel', 'GPTAPI', 'LMDeployClient',
+    'BaseModel', 'BaseAPIModel', 'SENSENOVA_API', 'GPTAPI', 'LMDeployClient',
     'LMDeployPipeline', 'LMDeployServer', 'HFTransformer',
     'HFTransformerCasualLM', 'INTERNLM2_META', 'HFTransformerChat', 'VllmModel'
 ]
