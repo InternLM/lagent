a656e604d283654c44b10c154d70b3daa4f797f7
braisedpork1964 <497494458@qq.com>
2025-07-01 07:31:48 +0000

    update examples

--- examples/run_async_agent_api_model_with_mcp.py
+++ examples/run_async_agent_api_model_with_mcp.py
@@ -1,10 +1,5 @@
 import asyncio
 import json
-import os
-import time
-from typing import List
-
-from volcenginesdkarkruntime import Ark, AsyncArk
 
 from lagent.actions import AsyncActionExecutor, AsyncMCPClient
 from lagent.agents import AsyncAgentForInternLM
@@ -14,75 +9,12 @@ from lagent.llms import AsyncGPTAPI
 from lagent.prompts import PluginParser
 from lagent.schema import AgentMessage
 
-
-class LcAsyncAPI(AsyncGPTAPI):
-    def __init__(self, model, api_key=None, max_tokens=4096, max_retries=4, **gen_params):
-        if api_key is None:
-            raise ValueError("api_key is required")
-        self.model = model
-        self.max_tokens = max_tokens
-        self.retry = max_retries
-        self.client = AsyncArk(
-            api_key=api_key, timeout=900, max_retries=self.retry, base_url='https://ark.cn-beijing.volces.com/api/v3'
-        )
-        super().__init__(**gen_params)
-
-    async def _chat(self, messages: List[dict], **gen_params) -> str:
-        """Generate completion from a list of templates.
-
-        Args:
-            messages (List[dict]): a list of prompt dictionaries
-            gen_params: additional generation configuration
-
-        Returns:
-            str: The generated string.
-        """
-        assert isinstance(messages, list)
-
-        max_num_retries = 0
-        while max_num_retries < self.retry:
-            try:
-                response = await self.client.chat.completions.create(
-                    model=self.model,
-                    messages=messages,
-                    max_tokens=self.max_tokens,
-                    temperature=gen_params.get('temperature', 0.8),
-                    top_p=gen_params.get('top_p', 0.95),
-                    stream=True,
-                )
-                reasoning_content = ""
-                content = ""
-                async for chunk in response:
-                    if (
-                        hasattr(chunk.choices[0].delta, 'reasoning_content')
-                        and chunk.choices[0].delta.reasoning_content
-                    ):
-                        reasoning_content += chunk.choices[0].delta.reasoning_content
-                        # print(chunk.choices[0].delta.reasoning_content, end="")
-                    else:
-                        content += chunk.choices[0].delta.content
-                        # print(chunk.choices[0].delta.content, end="")
-                # response = json.loads(response.json())
-                # reasoning_content = response['choices'][0]['message']['reasoning_content'].strip()
-                # content = response['choices'][0]['message']['content'].strip()
-                return content if reasoning_content == "" else "<think>" + reasoning_content + "</think>\n" + content
-
-            except Exception as error:
-                self.logger.error(str(error))
-                time.sleep(20)
-            max_num_retries += 1
-
-        raise RuntimeError(
-            'Calling OpenAI failed after retrying for ' f'{max_num_retries} times. Check the logs for ' 'details.'
-        )
-
-
 TEMPLATE = (
     "You have access to the following tools:\n{tool_description}\nPlease provide"
     " your thought process when you need to use a tool, followed by the call statement in this format:"
     "\n{invocation_format}"
 )
-llm = dict(type=LcAsyncAPI, model=None, api_key=None, top_p=0.95, temperature=0.6, max_tokens=16384, max_retries=50)
+llm = dict(type=AsyncGPTAPI, model_type=None, retry=50, key=None, top_p=0.95, temperature=0.6, max_new_tokens=16384)
 plugin = dict(
     type=AsyncMCPClient,
     name='PlayWright',
