1b423e5a9fc7b21ea4f6101be3b10a4d1157dce2
liukuikun <24622904+Harold-lkk@users.noreply.github.com>
2026-05-26 21:38:27 +0800

    Update lagent/serving/sandbox/daemon.py

--- lagent/serving/sandbox/daemon.py
+++ lagent/serving/sandbox/daemon.py
@@ -355,7 +355,7 @@ class AgentDaemon(BaseDaemon):
 
         if cmd == 'get_messages':
             try:
-                return {'messages': self.agent.get_messages()}
+                return self.agent.get_messages()
             except Exception as e:
                 return {"error": str(e)}
 
